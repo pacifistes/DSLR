452e0de4d66eaf6e37a11ef3620b5816fb2e8fc8
pacifistes <bebebe11>
2018-11-19 12:16:11 +0100

    verif de l'option describe

--- describe.py
+++ describe.py
@@ -11,6 +11,8 @@ def describe(datas):
 	dictDescribeData = {}
 	indexNames = {0 : "count", 1 : "mean", 2 : "std", 3 : "min", 4 : "25%", 5 : "50%", 6 : "75%", 7 : "max"}
 	for data in datas:
+		if (data == 'Hogwarts House'):
+			continue
 		dictDescribeData.update({data : describeColumn(datas[data].values)})
 	describeDatas = pd.DataFrame(dictDescribeData)
 	describeDatas.rename(index = indexNames, inplace = True)
@@ -19,19 +21,18 @@ def describe(datas):
 def main():
 	vlen = len(sys.argv)
 	if (vlen == 2 or (vlen == 3 and sys.argv[1] == "-byHouse")):
-		datas = csv.readCSVFile(sys.argv[1], ',')
+		datas = csv.readCSVFile(sys.argv[vlen - 1], ',')
 		if (datas is None):
 			sys.exit(1)
-		subjectDatas = csv.dropUselessColumn(datas, True)
+		subjectDatas = csv.dropUselessColumn(datas, False)
 		if (subjectDatas is None):
 			sys.exit(1)
 		# print subjectDatas.describe()
-		# print "\n"
-			# tmpList.append(dropNa(datas[datas['Hogwarts House'].isin({house})][data].values))
 		print "Describe general:\n{}\n".format(describe(subjectDatas))
 		if (vlen == 3):
 			for house in csv.houseNames:
-				print "Describe {}:\n{}\n".format(house, describe(subjectDatas.loc[[subjectDatas['Hogwarts House'].isin({house})]]))
+				print "Describe {}:\n{}\n".format(house, describe(subjectDatas.loc[subjectDatas['Hogwarts House'].isin({house})]))
+
 	else:
 		print 'Error script : python describe.py -byHouse file.'
 
