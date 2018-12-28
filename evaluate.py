f2250cf77a4c17b333379f992e0e3e9ac4e6722c
pacifistes <bebebe11>
2018-12-28 16:18:52 +0100

    modification du fichier evaluate.py + preparation des functions pour le programmes predict

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
+from __future__ import division
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score
 
 
 if __name__ == '__main__':
@@ -10,7 +10,12 @@ if __name__ == '__main__':
 	predictions = pd.read_csv('ressources/houses.csv', sep=',', index_col=0)
 	# Replace names by numerical value {0, 1, 2, 3} and convert to array
 	houses = {'Gryffindor': 0, 'Hufflepuff': 1, 'Ravenclaw': 2, 'Slytherin': 3}
-	y_true = truths.replace(houses).as_matrix()
-	y_pred = predictions.replace(houses).as_matrix()
-	# Print the score using accuracy_score
-	print("Your score on test set: {}".format(accuracy_score(y_true.reshape((400, )), y_pred.reshape((400, )))))
+	y_true = truths.replace(houses).values
+	y_pred = predictions.replace(houses).values
+	print(len(y_true))
+	print(len(y_pred))
+	totalTrue = 0
+	for true, predict in zip(y_true, y_pred):
+		if (true == predict):
+			totalTrue += 1
+	print('prediction :' + str(totalTrue / len(y_true)))
--- logreg_train.py
+++ logreg_train.py
@@ -11,20 +11,20 @@ import numpy as np
 g_featureColumns = ['Astronomy','Herbology','Divination','Muggle Studies','Ancient Runes','History of Magic']
 
 class	LogisticRegression:
-	def __init__(self, trainFilename, classifyColumn, featureColumns, predictFilename=None):
+	def __init__(self, trainFilename, classifyColumn, featureColumns, predictFilename = None):
 		self.trainFilename = trainFilename
-		self.predictFilename = predicFilename
+		self.predictFilename = predictFilename
 		self.classifyColumn = classifyColumn
 		self.featureColumns = list(featureColumns)
 		self.features = None
 		self.predicFeatures = None
 		self.classes = None
 		self.thetas = None
-		self.__minimums = None
-		self.__maximums = None
-		self.__means = None
+		self.__minimums = []
+		self.__maximums = []
+		self.__means = []
 		self.__learningRate = 0.3
-		self.__numberIteration = 250
+		self.__numberIteration = 2500
 		self.__isInitDoneSuccessfully = False
 	
 	def __normalizeLambda(self, minimum, maximum):
@@ -53,9 +53,9 @@ class	LogisticRegression:
 		newFeatures = {}
 		for feature, iFeature in zip(features, range(features.shape[1])):
 			values = features[feature].values
-			minimum = self.__minimum[iFeature]
-			maximum = self.__maximum[iFeature]
-			mean = self.__mean[iFeature]
+			minimum = self.__minimums[iFeature]
+			maximum = self.__maximums[iFeature]
+			mean = self.__means[iFeature]
 			normalizeLambda = self.__normalizeLambda(minimum, maximum)
 			func = partial(self.__applyNormalize ,mean, normalizeLambda)
 			newFeatures.update({feature : map(func, features[feature].values)})
@@ -112,17 +112,25 @@ class	LogisticRegression:
 			return False
 		try:
 			self.features = self.__normalize(datas[self.featureColumns])
-			if (self.predictFilename != None):
-				datas = csv.readCSVFile(self.predicFilename , ',')
-				if (datas is None):
-					return False
-				self.predicFeatures = self.__normalizePredictFile(datas[self.featureColumns])
 		except Exception:
 			print("One or multiple columns beetween: ", ", ".join(self.featureColumns), " doesn't exits")
 		try:
 			self.classes = datas[self.classifyColumn]
 		except Exception:
 			print("the classify column ", self.classifyColumn, "doesn't exits")
+		
+
+		try:
+			if (self.predictFilename != None):
+				datas = csv.readCSVFile(self.predictFilename , ',')
+				if (datas is None):
+					return False
+				self.predicFeatures = self.__normalizePredictFile(datas[self.featureColumns])
+		except Exception:
+			print("One or multiple columns beetween: ", ", ".join(self.featureColumns), " doesn't exits")
+
+
+
 		if (all(variable is not None for variable in [self.features, self.classes])):
 			self.__isInitDoneSuccessfully = True
 			return True
@@ -151,11 +159,27 @@ class	LogisticRegression:
 		if (self.predictFilename is None):
 			print("Add the predict filename in constructor")
 			return None
-		datas = csv.readCSVFile(self.predicFilename , ',')
+		datas = csv.readCSVFile("thetas.csv" , ',')
 		if (datas is None):
 			return False
-		thetas = datas['thetas0' + self.featureColumns]
-		print(thetas)
+		# print(datas)
+		try:
+			thetas = datas[['theta0'] + self.featureColumns]
+		except Exception:
+			print("Error in thetas File")
+			return None
+		# print('**********')
+		classNames = self.classes.unique()
+		# print(classNames)
+		# for lineThetas in thetas.values:
+			# print(lineThetas)
+		for values in self.predicFeatures.values:
+			results = [self.__predict(lineThetas, values.tolist()) for lineThetas in thetas.values]
+			predicts.append(classNames[results.index(max(results)) - 1])
+		
+		dataframe = {}
+		dataframe.update({self.classifyColumn : predicts})
+		return pd.DataFrame(dataframe)
 
 	def writeThetas(self):
 		if (self.__isInitDoneSuccessfully is False):
@@ -163,12 +187,15 @@ class	LogisticRegression:
 			return
 		csv.writeCSVFile('thetas.csv', self.thetas)
 
-
 def main():
 	vlen = len(sys.argv)
 	if (vlen == 2):
-		logistic = LogisticRegression('ressources/dataset_train.csv', 'Hogwarts House', g_featureColumns)
+		logistic = LogisticRegression('ressources/dataset_train.csv', 'Hogwarts House', g_featureColumns, 'ressources/dataset_test.csv')
 		if (logistic.init() and logistic.train()):
+		# if (logistic.init()):
+			# result = logistic.predictAll()
+			# if (result is not None):
+				# result.to_csv('ressources/houses.csv', sep=',', index=True, index_label='Index')
 			logistic.writeThetas()
 	else:
 		print("Error script : python logreg_train.py file.")
